f92203a250b316bfb0c8e370d0462ab2c60b143b
Chirag Dodiya <cod@kanakinfosystems.com>
2023-12-28 16:58:05 +0530

    Update sale_barcode_print.py

--- sale_barcode_printing/sale_barcode_printing/doctype/sale_barcode_print/sale_barcode_print.py
+++ sale_barcode_printing/sale_barcode_printing/doctype/sale_barcode_print/sale_barcode_print.py
@@ -13,17 +13,16 @@ class SaleBarcodePrint(Document):
 
     @frappe.whitelist()
     def get_workorders(self):
-        if not self.barcode_details:
-            barcode_details = []
-            for wo in frappe.get_list("Work Order", fields=["name as name"], filters=[["sales_order", "=", self.sales_order]]):
-                work_order = frappe.get_doc("Work Order", wo.get('name'))
-                barcode_details.append({
-                    'product': work_order.item_name,
-                    'item_code': work_order.production_item,
-                    'qty': work_order.qty,
-                    'work_order': work_order.name
-                })
-            self.update({'barcode_details':barcode_details})
+        barcode_details = []
+        for wo in frappe.get_list("Work Order", fields=["name as name"], filters=[["sales_order", "=", self.sales_order]]):
+            work_order = frappe.get_doc("Work Order", wo.get('name'))
+            barcode_details.append({
+                'product': work_order.item_name,
+                'item_code': work_order.production_item,
+                'qty': work_order.qty,
+                'work_order': work_order.name
+            })
+        self.update({'barcode_details':barcode_details})
 
     @frappe.whitelist()
     def print_labels(self):
