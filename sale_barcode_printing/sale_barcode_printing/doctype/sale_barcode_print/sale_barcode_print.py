a80d6b0151b25c7fb2694167efcdc6824b28fe05
Kanak Infosystems LLP <info@kanakinfosystems.com>
2023-12-28 16:31:54 +0530

    Update sale_barcode_print.py

--- sale_barcode_printing/sale_barcode_printing/doctype/sale_barcode_print/sale_barcode_print.py
+++ sale_barcode_printing/sale_barcode_printing/doctype/sale_barcode_print/sale_barcode_print.py
@@ -16,14 +16,15 @@ class SaleBarcodePrint(Document):
         barcode_details = []
         for wo in frappe.get_list("Work Order", fields=["name as name"], filters=[["sales_order", "=", self.sales_order]]):
             work_order = frappe.get_doc("Work Order", wo.get('name'))
-            barcode_details.append({
-                'product': work_order.item_name,
-                'item_code': work_order.production_item,
-                'qty': work_order.qty,
-                'work_order': work_order.name
-            })
-        self.update({'barcode_details':barcode_details})
-        self.save()
+            self.append('barcode_details', dict(product=work_order.item_name, item_code=work_order.production_item, qty=work_order.qty,work_order=work_order.name))
+            # barcode_details.append({
+            #     'product': work_order.item_name,
+            #     'item_code': work_order.production_item,
+            #     'qty': work_order.qty,
+            #     'work_order': work_order.name
+            # })
+        # self.update({'barcode_details':barcode_details})
+        # self.save()
 
     @frappe.whitelist()
     def print_labels(self):
