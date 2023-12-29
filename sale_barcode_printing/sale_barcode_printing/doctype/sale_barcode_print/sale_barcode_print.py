66476dd1b5f6f430f0f73065614da19f4bc697b2
ndm-kanak <49303030+ndm-kanak@users.noreply.github.com>
2023-12-29 15:35:09 +0530

    Update sale_barcode_print.py

--- sale_barcode_printing/sale_barcode_printing/doctype/sale_barcode_print/sale_barcode_print.py
+++ sale_barcode_printing/sale_barcode_printing/doctype/sale_barcode_print/sale_barcode_print.py
@@ -11,24 +11,28 @@ from frappe.core.utils import html2text
 
 class SaleBarcodePrint(Document):
 
+    def validate(self):
+        barcode_details = []
+        for wo in frappe.get_list("Work Order", fields=["name as name"], filters=[["sales_order", "=", self.sales_order]]):
+            work_order = frappe.get_doc("Work Order", wo.get('name'))
+            row = self.append('barcode_details', {})
+            row.product = work_order.item_name
+            row.item_code = work_order.production_item
+            row.qty = work_order.qty
+            row.work_order = work_order.name
+
+    @frappe.whitelist()
     def get_workorders(self):
         barcode_details = []
-        # for wo in frappe.get_list("Work Order", fields=["name as name"], filters=[["sales_order", "=", self.sales_order]]):
-        #     work_order = frappe.get_doc("Work Order", wo.get('name'))
-        #     # barcode_details.append({
-        #     #     'product': work_order.item_name,
-        #     #     'item_code': work_order.production_item,
-        #     #     'qty': work_order.qty,
-        #     #     'work_order': work_order.name
-        #     # })
-        if not self.barcode_details:
-            self.append("barcode_details", {
+        for wo in frappe.get_list("Work Order", fields=["name as name"], filters=[["sales_order", "=", self.sales_order]]):
+            work_order = frappe.get_doc("Work Order", wo.get('name'))
+            barcode_details.append({
                 'product': work_order.item_name,
                 'item_code': work_order.production_item,
                 'qty': work_order.qty,
                 'work_order': work_order.name
             })
-            frappe.db.commit()
+        
 
     @frappe.whitelist()
     def print_labels(self):
