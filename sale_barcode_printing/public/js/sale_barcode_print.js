71c5eebdea6e7993321e10bdc4c4c31d87388d8e
ndm-kanak <49303030+ndm-kanak@users.noreply.github.com>
2024-01-02 13:36:29 +0530

    Update sale_barcode_print.js

--- sale_barcode_printing/public/js/sale_barcode_print.js
+++ sale_barcode_printing/public/js/sale_barcode_print.js
@@ -1,11 +1,11 @@
-frappe.ui.form.on("Sale Barcode Print", {
-    sales_order(frm) {
-        frappe.call({
-            method: "get_workorders",
-            doc: frm.doc,
-            callback: (r) => {
-                if (!r.exc) refresh_field("barcode_details");
-            },
-        });
-    }
-})
+// frappe.ui.form.on("Sale Barcode Print", {
+//     sales_order(frm) {
+//         frappe.call({
+//             method: "get_workorders",
+//             doc: frm.doc,
+//             callback: (r) => {
+//                 if (!r.exc) refresh_field("barcode_details");
+//             },
+//         });
+//     }
+// })
